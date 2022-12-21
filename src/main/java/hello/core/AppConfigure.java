86b53579365a739d6b0d3fae6df5048bad28e899
mingyu <mingyu.oh15b@gmail.com>
2022-12-21 17:29:57 +0900

    AppConfigure 리팩터링

--- src/main/java/hello/core/AppConfigure.java
+++ src/main/java/hello/core/AppConfigure.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,13 +12,22 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfigure {
     /* 실제 구현객체를 생성하고 생성자 주입
     *  의존관계에 대한 고민을 AppConfigure 가 담당.
-    *   DIP 완성 -> 구현체는 추상에만 의존하면 된다.*/
+    *  DIP 완성 -> 구현체는 추상에만 의존하면 된다.
+    *  역확과 구현이 드러나도록 리팩토링*/
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memeberRepository());
+    }
+
+    private MemberRepository memeberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memeberRepository(), discontPolicy());
+    }
+
+    private DiscountPolicy discontPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
